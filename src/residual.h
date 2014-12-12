a9e2b5fc73b231593a0a3f3de4798d666c56f6c5
huanghaoyu <hy.huang@foxmail.com>
2014-12-12 08:28:19 +0800

    Use DCT coefficients to get HOG

--- src/residual.h
+++ src/residual.h
@@ -10,7 +10,7 @@
 
 struct Residual
 {
-    int gridStep;
+    static const int gridStep = 16;
     bool firstFlag;
     Frame preFrame;
     Frame curFrame;
@@ -19,7 +19,7 @@ struct Residual
     Mat residualFrame;
     Mat residual;
 
-    Residual(int gridStep = 16) : gridStep(gridStep), firstFlag(true)
+    Residual() : firstFlag(true)
     {
     }
 
@@ -44,7 +44,8 @@ struct Residual
             preFrame = curFrame = frame;
             preRawImageGray = curRawImageGray;
             firstFlag = false;
-            frame.rsd = Mat::ones(frame.Dx.rows, frame.Dx.cols, CV_8UC1);
+//            frame.rsd = Mat::ones(frame.Dx.rows, frame.Dx.cols, CV_8UC1);
+            frame.rsd = Mat::zeros(curRawImageGray.rows, curRawImageGray.cols, CV_16SC1);
             return;
         }
 
@@ -52,8 +53,10 @@ struct Residual
         curFrame = frame;
         preRawImageGray = curRawImageGray;
 
-        residual = Mat::zeros(preFrame.Dx.rows, preFrame.Dx.cols, CV_MAKETYPE(curRawImageGray.depth(), curRawImageGray.channels()));
-        residualFrame = Mat::zeros(preRawImageGray.rows, preRawImageGray.cols, CV_MAKETYPE(curRawImageGray.depth(), curRawImageGray.channels()));
+//        residual = Mat::zeros(preFrame.Dx.rows, preFrame.Dx.cols, CV_MAKETYPE(curRawImageGray.depth(), curRawImageGray.channels()));
+//        residualFrame = Mat::zeros(preRawImageGray.rows, preRawImageGray.cols, CV_MAKETYPE(curRawImageGray.depth(), curRawImageGray.channels()));
+        residual = Mat::zeros(preRawImageGray.rows, preRawImageGray.cols, CV_16SC1);
+        residualFrame = Mat::zeros(preRawImageGray.rows, preRawImageGray.cols, CV_32FC1);
 
         for(int blk_j = 0; blk_j < preFrame.Dx.rows; ++blk_j)
         {
@@ -72,22 +75,55 @@ struct Residual
 //                        residualFrame.at<int8_t>(blk_j*gridStep+j, blk_i*gridStep+i)
 //                                = curRawImageGray.at<int8_t>(next_blk_j*gridStep+j, next_blk_i*gridStep+i)
 //                                    - preRawImageGray.at<int8_t>(blk_j*gridStep+j, blk_i*gridStep+i);
-                        sum += (curRawImageGray.at<int8_t>(next_blk_j*gridStep+j, next_blk_i*gridStep+i)
-                                - preRawImageGray.at<int8_t>(blk_j*gridStep+j, blk_i*gridStep+i));
+                        residualFrame.at<float>(blk_j*gridStep+j, blk_i*gridStep+i)
+                                = float(curRawImageGray.at<int8_t>(next_blk_j*gridStep+j, next_blk_i*gridStep+i) - preRawImageGray.at<int8_t>(blk_j*gridStep+j, blk_i*gridStep+i));
+//                        sum += (curRawImageGray.at<int8_t>(next_blk_j*gridStep+j, next_blk_i*gridStep+i)
+//                                - preRawImageGray.at<int8_t>(blk_j*gridStep+j, blk_i*gridStep+i));
                     }
                 }
-//                for(int j = 0; j < gridStep; ++j)
-//                {
-//                    for(int i = 0; i < gridStep; ++i)
-//                    {
-//                        residualFrame.at<int8_t>(blk_j*gridStep+j, blk_i*gridStep+i) = sum/(gridStep*gridStep);
-//                    }
-//                }
-                residual.at<int8_t>(blk_j, blk_i) = sum/(gridStep*gridStep);
+//                residual.at<int8_t>(blk_j, blk_i) = sum/(gridStep*gridStep);
+            }
+        }
+
+        for(int blk_j = 0; blk_j < preFrame.Dx.rows; ++blk_j)
+        {
+            for(int blk_i = 0; blk_i < preFrame.Dx.cols; ++blk_i)
+            {
+                Mat block(gridStep, gridStep, CV_32FC1);
+                Mat trans_block(gridStep, gridStep, CV_16SC1);
+
+                for(int j = 0; j < gridStep; ++j)
+                    for(int i = 0; i < gridStep; ++i)
+                        block.at<float>(j, i) = residualFrame.at<float>(blk_j*gridStep+j, blk_i*gridStep+i);
+
+                dct(block, trans_block);
+                trans_block = Quantize(trans_block);
+
+                for(int j = 0; j < gridStep; ++j)
+                    for(int i = 0; i < gridStep; ++i)
+                        residual.at<int16_t>(blk_j*gridStep+j, blk_i*gridStep+i) = trans_block.at<int16_t>(j, i);
             }
         }
 
         frame.rsd = residual.clone();
+    }
+
+    Mat Quantize(const Mat& src)
+    {
+        Mat dst(src.rows, src.cols, CV_16SC1);
+        for(int j = 0; j < src.rows; ++j)
+        {
+            for(int i = 0; i < src.cols; ++i)
+            {
+                if(src.at<float>(j, i) < 0)
+                    dst.at<int16_t>(j, i) = 0;
+                else if(src.at<float>(j, i) > 255.0)
+                    dst.at<int16_t>(j, i) = 255;
+                else
+                    dst.at<int16_t>(j, i) = int16_t(src.at<float>(j, i));
+            }
+        }
+        return dst;
     }
 };
 
